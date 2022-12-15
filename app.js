e73bff87c686dd1a4bee50ae6d3fde7782057f42
Will Barrett <otterly456@outlook.com>
2022-12-15 18:46:23 +0000

    Implement makeChoice function

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 
 const randYear = () => {
    let year = Math.floor(Math.random() * (2022 - -1000) + -1000);
-
-   if(year < 0){
+    if(year < 0){
     year *= -1;
     return `${year} BC`
    }
@@ -15,11 +14,28 @@ const randYear = () => {
 /* Object holding the two arrays with the 'Event' and 'Weather' responses */
 
 const outcomes = {
-    events: ["War broke out", "Peace was declared", "Nothing of interest Happened", 
-    "Everybody had a nice day", "There was a plague", "Time travel was invented again", 
-    "The King was deposed", "The Queen was deposed", "The Peasants revolted", 
-    "Everyone had the day off"],
-    weather: ["Sunny", "Rainy", "Sleety", "Windy", "Stormy", "Humid", "Snowy", "Foggy"]
+    events: ["war broke out", "peace was declared", "nothing of interest Happened", 
+    "everybody had a nice day", "there was a plague", "time travel was invented again", 
+    "the King was deposed", "the Queen was deposed", "the Peasants revolted", 
+    "tveryone had the day off"],
+    weather: ["sunny", "rainy", "sleety", "windy", "stormy", "humid", "snowy", 
+    "foggy", "boiling", "freezing"]
 }
 
+//Function to pick a number between 0-9
 
+const randNum = () => {
+    return Math.floor(Math.random() * 9)
+}
+
+//Function which returns a year, event and weather selection
+
+const makeChoice = () => {
+    let year = randYear();
+    let event = outcomes.events[randNum()];
+    let weather = outcomes.weather[randNum()];
+
+    return `On this day in the year ${year}, ${event}. Also, it was ${weather}`
+}
+
+console.log(makeChoice())
